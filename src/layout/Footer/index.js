d18830083665d50843602d256b6a0fc808981fd5
“Igormirowski” <igorpmirowski@gmail.com>
2022-07-15 14:38:57 +0100

    add layout: header adn footer

---
+++ src/layout/Footer/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import './style.css';
+
+const Footer = () => <span id="footer">Footer</span>
+
+export default Footer;
---
+++ src/layout/Header/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './style.css'
+
+const Header = () => {
+    return (
+
+    <nav>
+        {/* NAVBAR */}
+    </nav>
+       
+    
+    )
+    
+}
+
+export default Header;
