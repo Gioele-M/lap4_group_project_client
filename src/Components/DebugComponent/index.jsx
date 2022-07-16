fe5be7d7699a37f9ef8a1ed8c632017b6b40db2c
mabus <m.buscaroli@outlook.com>
2022-07-16 10:36:02 +0100

    implement draft of autoscrolling function for the notes text

--- src/Components/DebugComponent/index.jsx
+++ src/Components/DebugComponent/index.jsx
@@ -1,6 +1,7 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { fetchDebug, fetchMedia, loginUser } from '../../State/actionCreators'
+
 
 import styles from './index.module.css'
 
@@ -12,6 +13,26 @@ function DebugComponent() {
 
   const dispatch = useDispatch()
 
+  
+
+  useEffect(() => {
+    const scrollNote = document.querySelector('#scrollNote')
+    console.log('* * * ', scrollNote.scrollTop)
+    let prevState = 0
+    const intervalRef = setInterval(() => {
+      
+      let currState = scrollNote.scrollTop += 1
+      scrollNote.scrollTop = currState
+
+      if (currState === prevState) clearInterval(intervalRef)
+
+      prevState = currState
+      console.log(`currState is ${currState} and prevState is ${prevState}`)
+      
+    }, 100) 
+  }, [])
+  
+  
   return (
     <>
       <div
@@ -28,6 +49,17 @@ function DebugComponent() {
         src="https://www.youtube.com/embed/tgbNymZ7vqY?start=60&end=70">
       </iframe>
       {/* append to the address: ?mute=1&autoplay=1&loop=1&controls=0 */}
+
+      <div 
+        id="scrollNote"
+        className={styles.textBox}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae animi eum veritatis voluptatem amet quos vel nam dolores quo. Odio ducimus cumque sequi assumenda ipsum, qui nemo nisi voluptatem. Quisquam.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim dicta quisquam eveniet accusamus id vel, assumenda iusto explicabo quos similique ipsam tenetur neque qui, adipisci sequi totam nesciunt? Rem, nemo!
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae animi eum veritatis voluptatem amet quos vel nam dolores quo. Odio ducimus cumque sequi assumenda ipsum, qui nemo nisi voluptatem. Quisquam.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim dicta quisquam eveniet accusamus id vel, assumenda iusto explicabo quos similique ipsam tenetur neque qui, adipisci sequi totam nesciunt? Rem, nemo!
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae animi eum veritatis voluptatem amet quos vel nam dolores quo. Odio ducimus cumque sequi assumenda ipsum, qui nemo nisi voluptatem. Quisquam.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim dicta quisquam eveniet accusamus id vel, assumenda iusto explicabo quos similique ipsam tenetur neque qui, adipisci sequi totam nesciunt? Rem, nemo!
+      </div>
   </>
   )
 }
