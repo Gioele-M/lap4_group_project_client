a00f77651504239b13518b6b1b669cf46e1ee5a1
Tom Hughes <tomhughes1987@gmail.com>
2022-07-16 12:56:36 +0200

    FIX 'About' to 'AboutPage', ADD navibar placeholdr

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import { Routes, Route } from 'react-router-dom'
 import { HomePage, UserPage, PlaylistPage } from './Pages'
 import { DebugComponent } from './Components'
+import NavBar from './Components/NavBar/NavBar'
+import AboutPage from './Pages/AboutPage'
 
 function App() {
   return (
     <div className="App">
-      <p>Header</p>
+      <p>Header</p>.
+      <NavBar/>
       <Routes>
         <Route path="/" exact element={<HomePage />} />
         <Route path="/user" element={<UserPage />} />
         <Route path="/playlist" element={<PlaylistPage />} />
+        <Route path="/about" element={<AboutPage />} />
       </Routes>
       {/* Debug Component
       check browser console or Redux DevTools to chec the State */}
