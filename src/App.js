773ef8b80f4f43b72c8c53f337f881ed8fcee1d7
Tom Hughes <tomhughes1987@gmail.com>
2022-07-18 18:21:19 +0200

    refactor navbar/ modals (login.signup)

--- src/App.js
+++ src/App.js
@@ -4,14 +4,9 @@ import { HomePage, UserPage, PlaylistPage, AboutPage, SearchResultPage } from '.
 import NavBar from './Components/NavBar/NavBar'
 import { Footer } from './layout';
 import 'bootstrap/dist/css/bootstrap.min.css'
-import Signup from './Components/Signup';
 {/* <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin> */}
 <link href="https://fonts.googleapis.com/css2?family=Montserrat+Alternates:wght@500&display=swap" rel="stylesheet"></link>;
-//TOM ADD THE FOLLOWING CND'S FOR THE SIGNUP MODAL:
-{/* <script crossorigin src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-alpha1/dist/css/bootstrap.min.css"></script>;
-<script crossorigin src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>;
-<script crossorigin src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script> */}
 
 function App() {
   return (
@@ -23,7 +18,6 @@ function App() {
         <Route path="/playlist" element={<PlaylistPage />} />
         <Route path="/about" element={<AboutPage />} />
         <Route path="/search" element={<SearchResultPage />} />
-        <Route path="/signup" element={<Signup />} />
         {/* <Route path="/debug" element={<DebugComponent />} /> */}
       </Routes>
       <p>Footer</p>
--- src/Components/Modals_Signup_Login/ModalLogin.js
+++ src/Components/Modals_Signup_Login/ModalLogin.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
-import "./finalatempt.css";
+// import "./finalatempt.css";
 
 export default function ModalLogin(props) {
   return (
