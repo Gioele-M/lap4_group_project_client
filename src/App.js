095c387216a3773e4d362281e490002ab575e3f1
“Igormirowski” <igorpmirowski@gmail.com>
2022-07-18 07:54:05 +0100

    import footer to App

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import { Routes, Route } from 'react-router-dom'
 import { HomePage, UserPage, PlaylistPage, AboutPage } from './Pages'
 import { DebugComponent } from './Components'
 import NavBar from './Components/NavBar/NavBar'
+import { Footer } from './layout';
 import 'bootstrap/dist/css/bootstrap.min.css'
 {/* <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin> */}
 <link href="https://fonts.googleapis.com/css2?family=Montserrat+Alternates:wght@500&display=swap" rel="stylesheet"></link>
+
 
 function App() {
   return (
@@ -19,6 +21,7 @@ function App() {
         <Route path="/debug" element={<DebugComponent />} />
       </Routes>
       <p>Footer</p>
+      <Footer />
     </div>
   )
 }
