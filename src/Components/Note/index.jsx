e5dd93f57a85dde6f207498228a8f35023497ebd
mabus <m.buscaroli@outlook.com>
2022-07-19 17:47:32 +0100

    saving a stable state before working on code

--- src/Components/Note/index.jsx
+++ src/Components/Note/index.jsx
@@ -13,18 +13,37 @@ function Note(props) {
 
   const userData = useSelector(state => state.user.user)
   const mediaData = useSelector(state => state.media.data.data)
+  const [selected, setSelected] = useState(0)
 
   console.log('--- USER ---\n', userData)
   console.log('--- MEDIA --\n', mediaData)
 
   const handleSaveBtn = () => {
+    const chapters = mediaData[0].chapters
+
+    chapters.forEach(chap => {
+      if (chap.chapterId === props.chapterId) {
+        console.log('chapterId is: ', chap.chapterId)
+        console.log('link: ', chap.video_url)
+        chap.text = noteText
+        // chap.chaptertitle = noteChapterTitle
+        // chap.start = noteStart
+        // chap.end = noteEnd
+        chap.video_url = link
+        
+      }
+      
+    })
+
+    console.log('UPDATED CHAPTERS -----------------\n', chapters)
+
     const data = {
       "userRequesting":userData.userEmail,
-      "playlistName": mediaData.playlistName,
+      "playlistName": mediaData[0].playlistName,
       "chapters": mediaData.chapters,
       "token": userData.token
       }
-
+      console.log('SAVING NOTE: ', noteText)
       dispatch(patchMedia(data))
     }
 
@@ -34,7 +53,12 @@ function Note(props) {
       className={styles.noteWrapper}>
         <textarea
         data-testid="textArea" 
-          onChange={(e) => setNoteText(e.target.value)}
+          onChange={(e) => {
+            setNoteText(e.target.value)
+            setSelected(props.key)
+            console.log('ZZZZZ', noteText)
+            console.log('selected OoOoOo', selected)
+          }}
           className={styles.noteText} value={noteText || ''}/>
         <div className={styles.controls}>
           <div 
@@ -46,7 +70,7 @@ function Note(props) {
           className={styles.thumbnail}>Pic</div>
           <button 
             className={styles.saveBtn}
-            onClick={() => handleSaveBtn}
+            onClick={() => handleSaveBtn()}
           >Save</button>
           <button className={styles.deleteBtn}>Delete</button>
           <input 
