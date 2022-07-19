d0f8d7fffca29fc56a680a5d556f9564506d9ac2
Tom Hughes <tomhughes1987@gmail.com>
2022-07-19 22:26:57 +0200

    add sign up functionality

--- src/Components/Modals_Signup_Login/ModalSignup.js
+++ src/Components/Modals_Signup_Login/ModalSignup.js
@@ -9,15 +9,29 @@ import { signupUser } from "../../State/actionCreators/user";
 export default function ModalSignup(props) {
 
 
-  const [loginModalShow, setLoginModalShow] = React.useState(false);
+  const [loginModalShow, setLoginModalShow] = seState(false); user
 
-
-  
   const dispatch = useDispatch()
 
 
+  const [username, setUsername] = useState('Blank User:');
+  const [password, setPassword] = useState('Blank Password:');
+  const [email, setEmail] = useState('Blank Email');
 
-
+  function handleChangeUsername(event) {
+    setUsername({value: event.target.value});
+    console.log('username field:',username)
+  }
+  
+  function handleChangePassword(event) {
+    setPassword({value: event.target.value});
+    console.log('password field:',password)
+  }
+  
+  function handleChangeEmail(event) {
+    setEmail({value: event.target.value});
+    console.log('email field:',email)
+  }
 
 
   return (
@@ -31,19 +45,19 @@ export default function ModalSignup(props) {
         <div class="tab-pane my-1  Modal-signuplogin" id="signupSection" >
           <div class="form p-4  text-center Modal-signuplogin" id="modalstuff1">
           <p id='modal-intro'>Please enter you details:</p>
-            <input type="text" name="" class=" my-2 form-control" placeholder="User Name"/>
-            <input type="text" name="" class="my-2 form-control" placeholder="Email"/>
+            <input type="text" name="" class=" my-2 form-control" placeholder="User Name" onChange={handleChangeUsername}/>
+            <input type="text" name="" class="my-2 form-control" placeholder="Email" onChange={handleChangeEmail}/>
             <input type="text" name="" class="my-2   form-control" placeholder="Password"/>
-            <input type="text" name="" class=" my-2 form-control" placeholder="Repeat Password"/>
+            <input type="text" name="" class=" my-2 form-control" placeholder="Repeat Password" onChange={handleChangePassword}/>
 
 
 
             <button class="my-4 btn" id="btn-login" 
             onClick={() => 
             dispatch(signupUser(
-              {username: 'billieJeAN',
-              password: 'PASSword1234',
-              email: 'nunya@business.com', 
+              {username: username.value,
+              password: password.value,
+              email: email.value, 
           }))}>
               Sign Up
             </button>
