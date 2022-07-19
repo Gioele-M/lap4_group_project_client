07de4e64a1c2914f37e0be8ca25f4a5abdf31179
Tom Hughes <tomhughes1987@gmail.com>
2022-07-19 15:46:49 +0200

    attempting to dispatch from create new PL

--- src/Components/Modal_NewPlaylist/NewPlaylist.js
+++ src/Components/Modal_NewPlaylist/NewPlaylist.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState, useRef, useEffect} from "react";
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
 import "./styles.css";
@@ -7,28 +7,46 @@ export default function ModalLogin(props) {
   // const handleClose = () => setShow(false);
   const [signupModalShow, setSignupModalShow] = React.useState(false);
 
+  const colourRef = useRef()
+
+// console .log
+
+  const [colourPicker1, setColourPicker1] = useState('rgb(4,2,4)')
+
+  // function checkValue (){
+    console.log("rgbForm Value is:")
+    console.log(colourPicker1)
+
+    useEffect(() => {
+      // Update the document title using the browser API
+      console.log(colourPicker1)
+    });
+
+    // let rgbForm = colourPicker1
+    // let rgbForm = document.getElementsByClassName.colourPicker
+  // }
+  
   return (
     <Modal {...props} aria-labelledby="contained-modal-title-vcenter" centered  id="modalstuff" >
-
-
       <Modal.Header closeButton>
         <Modal.Title id="modalTitle">Create A Playlist</Modal.Title>
       </Modal.Header>
-
-      
-
       <div class="tab-pane my-1  Modal-signuplogin" id="signupSection" >
-      <div class="form p-4  text-center Modal-signuplogin" id="modalstuff1">
-
-      {/* <p id='modal-intro'>Please enter you details:</p> */}
-        <div class="form  p-2 Modal-signuplogin" id="signupSection">
-
+        <div class="form p-4  text-center Modal-signuplogin" id="modalstuff1">
+          {/* <p id='modal-intro'>Please enter you details:</p> */}
+            <div class="form  p-2 Modal-signuplogin" id="signupSection">
           <input type="text" name="" class="my-1 form-control" placeholder="Playlist Name:"/>
           {/* <input type="checkbox" name="" class="my-3 form-control" placeholder="Public"/> */}
           {/* <label for="colourPicker"> Pick a background colour:</label><br/> */}
-          <input type="color" name="" class="my-3 form-control " id='colourPicker' placeholder="Colour"/>
+          <input ref={colourRef} type="color" class="my-3 form-control colourPicker"  id='MyColourPicker' 
+          onClick={() => setColourPicker1(colourRef.value)} placeholder="Colour"/>
+          
+
+
+
           <input type="checkbox" id="privateCheck" name="privateCheck" value="private"></input>
           <label for="privateCheck"> Private</label><br/>
+  
 
           <button class="my-4 btn " id="btn-login" >Lets Go!</button>
 
