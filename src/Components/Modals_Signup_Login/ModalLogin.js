678b14e0c9c9bacc2ce5f5f09cc1ff290b3253cb
Tom Hughes <tomhughes1987@gmail.com>
2022-07-19 02:19:08 +0200

    update modals: login/ signup

--- src/Components/Modals_Signup_Login/ModalLogin.js
+++ src/Components/Modals_Signup_Login/ModalLogin.js
@@ -1,17 +1,52 @@
 import React from "react";
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
-// import "./finalatempt.css";
+import "./styles.css";
 
-export default function ModalLogin(props) {
+export default function NewPlaylist(props) {
+  // const handleClose = () => setShow(false);
+  const [signupModalShow, setSignupModalShow] = React.useState(false);
+
   return (
-    <Modal
-      {...props}
-      size="lg"
-      aria-labelledby="contained-modal-title-vcenter"
-      centered
-    >
-        <div>stuff</div>
-        </Modal>
+    <Modal {...props} aria-labelledby="contained-modal-title-vcenter" centered  id="modalstuff" >
+
+
+      <Modal.Header closeButton>
+        <Modal.Title id="modalTitle">Log in</Modal.Title>
+      </Modal.Header>
+
+      
+
+      <div class="tab-pane my-1  Modal-signuplogin" id="signupSection" >
+      <div class="form p-4  text-center Modal-signuplogin" id="modalstuff1">
+
+      <p id='modal-intro'>Please enter you details:</p>
+        <div class="form  p-2 Modal-signuplogin" id="signupSection">
+
+          <input type="text" name="" class="my-1 form-control" placeholder="Playlist Name"/>
+          <input type="text" name="" class="my-3 form-control" placeholder="Password"/>
+          
+
+          <button class="my-4 btn " id="btn-login">Log In</button>
+            <br/>
+              {/* <a id='already'  onClick={handleClose}  href='#'>Already have an account?</a> */}
+              <a id='already'  href='#'>Don't have an account?</a>
+
+
+
+
+
+
+
+
+
+
+          {/* onClick={() => setLoginModalShow(true)}  */}
+          {/* <button class="btn btn-dark btn-block" onClick={handleClose}>Signup</button> */}
+          
+        </div>
+      </div>
+      </div>
+    </Modal>
   )
 }
--- src/Components/Modals_Signup_Login/ModalSignup.js
+++ src/Components/Modals_Signup_Login/ModalSignup.js
@@ -1,24 +1,45 @@
 import React from "react";
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
-// import "./finalatempt.css";
+import "./styles.css";
+import ModalLogin from '../Modals_Signup_Login/ModalLogin';
 
 export default function ModalSignup(props) {
+  // const [show, setShow] = useState(false);
+
+  // const handleClose = () => setShow(false);
+  const [loginModalShow, setLoginModalShow] = React.useState(false);
+
   return (
-    <Modal
-      {...props}
-      size="lg"
-      aria-labelledby="contained-modal-title-vcenter"
-      centered
-    >
-    <div>ModalSignup</div>
-    <div class="tab-pane" id="signupSection" >
-          <div class="form px-4">
-            <input type="text" name="" class="form-control" placeholder="Name"/>
-            <input type="text" name="" class="form-control" placeholder="Email"/>
-            <input type="text" name="" class="form-control" placeholder="Phone"/>
-            <input type="text" name="" class="form-control" placeholder="Password"/>
-            <button class="btn btn-dark btn-block">Signup</button>
+    <Modal {...props}  aria-labelledby="contained-modal-title-vcenter" centered id="modalstuff">
+     
+      <Modal.Header closeButton>
+        <Modal.Title id="modalTitle">Sign Up</Modal.Title>
+      </Modal.Header>
+      
+        {/* <div class="tab-pane Modal-signuplogin" id="signupSection" > */}
+        <div class="tab-pane my-1  Modal-signuplogin" id="signupSection" >
+          <div class="form p-4  text-center Modal-signuplogin" id="modalstuff1">
+          <p id='modal-intro'>Please enter you details:</p>
+            <input type="text" name="" class=" my-2 form-control" placeholder="User Name"/>
+            <input type="text" name="" class="my-2 form-control" placeholder="Email"/>
+            <input type="text" name="" class="my-2   form-control" placeholder="Password"/>
+            <input type="text" name="" class=" my-2 form-control" placeholder="Repeat Password"/>
+
+            <button class="my-4 btn " id="btn-login">Sign Up</button>
+            <br/>
+              {/* <a id='already'  onClick={handleClose}  href='#'>Already have an account?</a> */}
+              <a id='already' href='#'>Already have an account?</a>
+             
+              {/* <a id='already' onClick={() => setLoginModalShow(true)}  href='#'>Already have an account?</a> */}
+
+          {/* <ModalLogin  
+            show={loginModalShow}
+            onHide={() => setLoginModalShow(false)}
+          /> */}
+
+
+            {/* </div> */}
           </div>
         </div>
     </Modal>
