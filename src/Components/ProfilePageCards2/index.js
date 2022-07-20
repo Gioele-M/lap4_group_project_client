f5b6306c1e971f26ddaa69c1db14d5527120dfc1
“Igormirowski” <igorpmirowski@gmail.com>
2022-07-20 11:40:00 +0100

    add css to card

--- src/Components/ProfilePageCards2/index.js
+++ src/Components/ProfilePageCards2/index.js
@@ -5,6 +5,7 @@ import { useSelector } from 'react-redux'
 import { Container, Row, Col }  from 'react-bootstrap';
 import { useState } from 'react';
 // import { Navigate } from 'react-router-dom';
+import "./styles.css";
 
 
 const  ProfilePageCard2 = () => {
