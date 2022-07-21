67a1987a194d240f6c088d7b8865c8874d601587
“Igormirowski” <igorpmirowski@gmail.com>
2022-07-21 07:58:44 +0100

    minor style change

--- src/Components/AboutModal/index.js
+++ src/Components/AboutModal/index.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
+import "./style.css";
 
 function AboutModal() {
   const [show, setShow] = useState(false);
