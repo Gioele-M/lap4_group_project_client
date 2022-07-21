503a5b131013d2d9ef82acb5d8271ebd6082b900
mabus <m.buscaroli@outlook.com>
2022-07-21 18:33:06 +0100

    styled navbar with Tom

--- src/Components/NewPlaylistBtn/index.js
+++ src/Components/NewPlaylistBtn/index.js
@@ -1,21 +1,25 @@
 import React from 'react'
-import NewPlaylist from '../Modal_NewPlaylist/NewPlaylist';
-import Button from 'react-bootstrap/Button';
-
+import NewPlaylist from '../Modal_NewPlaylist/NewPlaylist'
+import Button from 'react-bootstrap/Button'
+import './styles.css'
 
 export default function NewPlaylistBtn() {
-
-    const [signupModalShow, setSignupModalShow] = React.useState(false);
-
+  const [signupModalShow, setSignupModalShow] = React.useState(false)
 
   return (
+    <>
+      <Button
+        className="btn btn-sm  btn-outline-success me-1 align-middle "
+        onClick={() => setSignupModalShow(true)}
+        id="btn-new-playlist"
+      >
+        ADD NEW PLAYLIST+
+      </Button>
 
-<>
-    <Button className="btn btn-sm  btn-outline-success me-1 align-middle " onClick={() => setSignupModalShow(true)} id='btn-login'>ADD NEW PLAYLIST+</Button>
-
-<NewPlaylist show={signupModalShow}
-    onHide={() => setSignupModalShow(false)}/>
-  </>  
-
+      <NewPlaylist
+        show={signupModalShow}
+        onHide={() => setSignupModalShow(false)}
+      />
+    </>
   )
 }
